38c5692eed56d071e7f1279eed9b3713ed5a17f9
Loago18-A <loagoo18@gmail.com>
2026-04-30 22:47:13 +0200

    Implement ScheduleService logic and MainApp switchboard

--- src/ub/csi142/studygroupscheduler/service/ScheduleService.java
+++ src/ub/csi142/studygroupscheduler/service/ScheduleService.java
@@ -1,18 +1,71 @@
 package ub.csi142.studygroupscheduler.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import ub.csi142.studygroupscheduler.exceptions.GroupNotFoundException;
+import ub.csi142.studygroupscheduler.model.Session;
+import ub.csi142.studygroupscheduler.model.StudyGroup;
+import ub.csi142.studygroupscheduler.model.TimeSlot;
 import ub.csi142.studygroupscheduler.util.InputHelper;
 
 public class ScheduleService {
 
+    private List<Session> sessions = new ArrayList<>();
+
     public void scheduleSession(InputHelper input, GroupService groupService) {
-        System.out.println("Scheduling session...");
+        System.out.println("\n--- Schedule a New Session ---");
+
+        try {
+            String groupName = input.readString("Enter the Study Group Name: ");
+
+            StudyGroup group = groupService.findGroupbyName(groupName);
+
+            String time = input.readString("Enter day and time(e.g. Friday 14:00): ");
+            TimeSlot slot = new TimeSlot(time);
+
+            Session newSession = new Session(group, slot);
+            sessions.add(newSession);
+
+            System.out.println("Success: Session scheduled for " + groupName);
+
+        } catch (GroupNotFoundException e) {
+
+            System.out.println("Error: " e.getMessage());
+        }
     }
 
     public void viewSessionsForGroup(InputHelper input, GroupService groupService) {
-        System.out.println("Viewing sessions for a group...");
+
+        String groupName = input.readString("Enter group name to filter by: ");
+        boolean found = false
+
+        System.out.println("\n--- Sessions for " + groupName + " ---");
+
+        for (Session s : sessions) {
+
+            if (s.getGroup().getName().equalsIgnoreCase(groupName)) {
+
+                System.out.println(s);
+                found = true
+                
+            }
+        }
+
+        if (!found) {
+            System.out.println("No sessions currently scheduled for this group.");
+        }
     }
 
     public void viewAllSessions() {
-        System.out.println("Displaying all sessions...");
+        System.out.println("\n--- All Scheduled Sessions ---");
+
+        if (sessions.isEmpty()) {
+            System.out.println("The schedule is currently empty.");
+        } else {
+            for (Session s : sessions) {
+
+                System.out.println(s);
+            }
+        }
     }
 }
